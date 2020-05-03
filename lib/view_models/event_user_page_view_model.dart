358ee57b47e005e1a7477fbe0dcab3a562b58f6b
manjunath-biji <61246594+manjunath-biji@users.noreply.github.com>
2020-05-03 16:45:32 +0530

    Done on 3rd May 2020
    ver2.0_Event

---
+++ lib/view_models/event_user_page_view_model.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter_kirthan/models/eventuser.dart';
+import 'package:flutter_kirthan/services/event_user_service_interface.dart';
+import 'package:meta/meta.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class EventUserPageViewModel extends Model {
+  Future<List<EventUser>> _eventUsers;
+  final IEventUserRestApi apiSvc;
+
+  EventUserPageViewModel({@required this.apiSvc});
+
+  Future<List<EventUser>> get eventUsers => _eventUsers;
+
+  set eventUsers(Future<List<EventUser>> value) {
+    _eventUsers = value;
+    notifyListeners();
+  }
+
+  Future<List<EventUser>> getEventTeamUserMappings(String eventMapping) {
+    Future<List<EventUser>> eventusers = apiSvc?.getEventTeamUserMappings(eventMapping);
+    return eventusers;
+  }
+
+  Future<List<EventUser>> submitNewEventTeamUserMapping(
+      List<EventUser> listofeventsermap) {
+    Future<List<EventUser>> eventusers = apiSvc?.submitNewEventTeamUserMapping(listofeventsermap);
+    return eventusers;
+
+  }
+
+  Future<List<EventUser>> submitDeleteEventTeamUserMapping(
+      List<EventUser> listofeventsermap) {
+    Future<List<EventUser>> eventusers = apiSvc?.submitDeleteEventTeamUserMapping (listofeventsermap);
+    return eventusers;
+
+  }
+}
