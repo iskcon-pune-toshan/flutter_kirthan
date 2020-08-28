836f198c1891ed12149e237e40b50acb25a0e362
Afrah Shaikh <afrah.17u278@viit.ac.in>
2020-08-28 23:31:50 +0530

    base Service

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -4,7 +4,7 @@ abstract class BaseAPIService {
   //final baseUrl = 'http://10.0.2.2:8080';
   //final baseUrl = 'http://192.168.1.7:8080'; // Janice
 
-  final baseUrl = 'http://192.168.1.7:8080'; //Manju
+  final baseUrl = 'http://169.254.255.240:8084'; //Manju
   http.Client client1 = http.Client();
 
   set client(http.Client value) => client1 = value;
