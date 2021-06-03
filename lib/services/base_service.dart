846df45f03617c8edaecc7a4bb227ffbdec51acf
NishaK1211 <nisha.khandelwal1225@gmail.com>
2021-06-03 10:30:47 +0530

    Hostname Change

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -1,11 +1,11 @@
 import 'package:http/http.dart' as http;
 
 abstract class BaseAPIService {
-  // final baseUrl = 'http://192.168.43.222:8080'; //Nisha
-  //final baseUrl = 'http://192.168.1.7:8080';    //Manjunath Sir
-  //final baseUrl = 'http://172.20.10.2:9090';  //Afrah
- final baseUrl = 'http://164.52.202.127:9090';  //server
- // final baseUrl = 'http://192.168.0.108:8080';  //Amruta
+ // final baseUrl = 'http://164.52.202.127:9090';  //server
+ final baseUrl = 'http://kirtanappprod.kairavalabs.com:8080'; // Production
+ // final baseUrl = 'http://kirtanappdev.kairavalabs.com:9090'; // Development
+
+
   http.Client client1 = http.Client();
 
   final int userId = 4;
