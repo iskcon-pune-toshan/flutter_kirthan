27408554ed83d265e7cc4d9aafda4459195eed8b
Afrah Shaikh <afrah.17u278@viit.ac.in>
2021-06-14 10:13:17 +0530

    Base services

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -1,8 +1,8 @@
 import 'package:http/http.dart' as http;
 
 abstract class BaseAPIService {
- // final baseUrl = 'http://164.52.202.127:9090';  //server
- final baseUrl = 'http://kirtanappprod.kairavalabs.com:8080'; // Production
+  final baseUrl = 'http://164.52.202.127:9090';  //server
+ //final baseUrl = 'http://kirtanappprod.kairavalabs.com:9090'; // Production
  // final baseUrl = 'http://kirtanappdev.kairavalabs.com:9090'; // Development
 
  http.Client client1 = http.Client();
